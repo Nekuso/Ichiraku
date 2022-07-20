24d401a6befa4158d7c1abb34d9850c54ba760d2
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-07-20 18:19:18 +0800

    Added files via upload

---
+++ src/components/Feature.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyledFeature} from './styles/Feature.styled';
+
+const Feature = () => {
+    return (
+        <StyledFeature>
+            <div className="feature__section">
+                
+            </div>
+        </StyledFeature>
+    );
+}
+
+export default Feature;
